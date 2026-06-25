4e2138b12c58065b27e7940d9356aee556a581b3
Muhaimin Khan <khnmuhaimin@MuhaimicBookNeo.lan>
2026-06-25 18:21:02 +0200

    Made a lot of changes

--- project/minimal/model_name.py
+++ project/minimal/model_name.py
@@ -2,3 +2,5 @@ MODEL_NAME = "base_monthly"
 # MODEL_NAME = "base_daily"
 # MODEL_NAME = "base_relvol_bsdelta_monthly"
 # MODEL_NAME = "base_relvol_bsdelta_daily"
+
+MODEL_NAMES = ["base_monthly", "base_daily", "base_relvol_bsdelta_monthly", "base_relvol_bsdelta_daily"]
